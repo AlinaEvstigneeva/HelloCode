d8f1825497ba4a01f9456c20dae4b974a647e083
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2022-11-25 16:58:15 +0300

    Add Seminar1_1

---
+++ Seminar1_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая  на вход принимает число,  выдает его квадат (число умноженное само на себя)
+
+// string number = Console.ReadLine();
+// Console.WriteLine(number);
+
+// int number = int.Parse(Console.ReadLine());
+// number +=1;
+// Console.WriteLine(number);
+
+
+int number = int.Parse(Console.ReadLine());
+
+int quarter = number * number;
+
+Console.WriteLine(quarter);
