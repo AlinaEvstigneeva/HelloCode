604e02f1455f85426c5152fd36cf0a07df1afbbf
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2022-11-30 18:22:28 +0300

    Added removal of the second digit

---
+++ Seminar2_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46 782 -> 72 918 -> 98
+
+Random randomNum = new();
+int number = randomNum.Next(100, 999);
+Console.WriteLine(number);
+
+int firstNumber = number / 100;
+int thirdNumber = number % 10;                  //чтобы работать с последней цифрой числа, всегда делим на 10 (%10)
+int result = firstNumber * 10 + thirdNumber;
+Console.WriteLine(result);
