02c3b2d9c89a29013a13a5e82f8e0d32b1c0ceed
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2022-11-30 21:17:26 +0300

    Added && to the condition

---
+++ Seminar2_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. 
+// 14 -> нет 46 -> нет 161 -> да
+
+int a = int.Parse(Console.ReadLine());
+int firstExam = 7;
+int secondExam = 23;
+
+if (a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
