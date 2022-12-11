9136d07d46662eb950c081d40e9a41474ada3b44
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2022-12-11 19:40:42 +0300

    Added Seminar 3_2

--- Seminar3_2/Program.cs
+++ Seminar3_2/Program.cs
@@ -1,3 +1,32 @@
 ﻿// Задача 18: Напишите программу, которая по заданному номеру
 // четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 
+int numberOfQuarter = int.Parse(Console.ReadLine());
+
+int possibleCoordinates = RangeOfPossibleCoordinates(numberOfQuarter);
+
+
+int RangeOfPossibleCoordinates( int numberOfQuarter)
+{
+    if (numberOfQuarter == 1)
+    {
+        Console.WriteLine(" x > 0 && y > 0 ");
+    }
+
+    if (numberOfQuarter == 2)
+    {
+        Console.WriteLine("x < 0 && y > 0");
+    }
+
+    if (numberOfQuarter == 3)
+    {
+        Console.WriteLine("x < 0 && y < 0");
+    }
+
+    if (numberOfQuarter == 4)
+    {
+        Console.WriteLine("x > 0 && y < 0");
+    }
+    return -1;
+}
+
