1691e9603852d26b2adcb8cc8dfdad5142a16944
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2022-12-15 22:52:26 +0300

    added adjustments

--- Seminar3_4/Program.cs
+++ Seminar3_4/Program.cs
@@ -17,12 +17,14 @@ int[] GetTableOfSquares(int num)
 
 int[] squaresTable = GetTableOfSquares(num);
 
+
 void PrintArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.WriteLine(array[i]);
+        Console.Write($"{array[i]} ");
     }
+    Console.WriteLine();
 }
 
 PrintArray(squaresTable);
