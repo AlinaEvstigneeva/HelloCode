1c8c88d7665b1473ac94586e0ead468f05be57a7
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2022-12-15 23:07:52 +0300

    added comments

--- Seminar3_4/Program.cs
+++ Seminar3_4/Program.cs
@@ -7,15 +7,16 @@ int num = int.Parse(Console.ReadLine());
 
 int[] GetTableOfSquares(int num)
 {
-    int[] array = new int[num];
-    for (int i = 0; i < num; i++)
+    int[] array = new int[num];                     // new int[] - массив, заполненный нулями
+    for (int i = 0; i < num; i++)                   // цикл идет по массиву и вписывает квадраты
     {
         array[i] = (int)Math.Pow(i + 1, 2);
     }
     return array;
 }
 
-int[] squaresTable = GetTableOfSquares(num);
+int[] squaresTable = GetTableOfSquares(num);        // заводим новый массив и применяем к нему метод GetTableOfSquares
+                                                    // в качестве переменной - num - размер массива. который ввел пользователь
 
 
 void PrintArray(int[] array)
