fa03eb20a7469f0e0ab6a694083ad0489c0ec79f
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2022-12-15 22:41:13 +0300

    added an array of squares

---
+++ Seminar3_4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 22: Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// int[] array =
+// 5 -> 1, 4, 9, 16, 25. 2 -> 1,4
+
+int num = int.Parse(Console.ReadLine());
+
+int[] GetTableOfSquares(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = (int)Math.Pow(i + 1, 2);
+    }
+    return array;
+}
+
+int[] squaresTable = GetTableOfSquares(num);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+PrintArray(squaresTable);
