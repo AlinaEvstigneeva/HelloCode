2421c1fa12772c2500bacde4896cbd99bf304be4
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2022-12-19 22:25:13 +0300

    added task 3

--- Seminar3_HW/Program.cs
+++ Seminar3_HW/Program.cs
@@ -1,2 +1,109 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+// Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль. Не использовать String!
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Console.Write("Укажите колличество цифр в числе: ");
+
+// int num = int.Parse(Console.ReadLine());
+
+
+// Console.Write("Введите число, написав каждую цифру через enter: ");
+
+// int dig1 = int.Parse(Console.ReadLine());
+// int dig2  = int.Parse(Console.ReadLine());
+// int dig3 = int.Parse(Console.ReadLine());
+// int dig4 = int.Parse(Console.ReadLine());
+// int dig5 = int.Parse(Console.ReadLine());
+
+// int [] GetArray(int num, int dig1, int dig2, int dig3, int dig4, int dig5)
+// {
+//     int [] array = new int [num];
+//     {
+//         for(int i = 0; i < num; i++)
+//         {
+//             array[i] = 
+//         }
+
+//     }
+// }
+
+
+
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве. Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Console.WriteLine("Введите координаты первой точки");
+// double pointA1 = Convert.ToDouble(Console.ReadLine());
+// double pointA2 = Convert.ToDouble(Console.ReadLine());
+// double pointA3 = Convert.ToDouble(Console.ReadLine());
+// double x1 = pointA1;
+// double y1 = pointA2;
+// double z1 = pointA3;
+
+
+// Console.WriteLine("Введите координаты второй точки");
+// double pointB1 = Convert.ToDouble(Console.ReadLine());
+// double pointB2 = Convert.ToDouble(Console.ReadLine());
+// double pointB3 = Convert.ToDouble(Console.ReadLine());
+
+// double x2 = pointB1;
+// double y2 = pointB2;
+// double z2 = pointB3;
+
+
+// double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+// double result = Math.Round(distance, 2);
+// Console.WriteLine($"Расстояние между двумя точками на плоскости равно {result}");
+
+
+
+
+
+
+
+
+
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+// Задание должно быть выполнено в методе и метод должен вернуть массив чисел.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+//  int num = int.Parse(Console.ReadLine());
+
+//  int[] cubeTable = GetTableOfCube(num);
+
+//  PrintArray(cubeTable);
+
+
+
+//  int[] GetTableOfCube(int num)
+// // {
+// //     int[] array = new int[num];                     
+// //     for (int i = 0; i < num; i++)                   
+// //     {
+// //         array[i] = (int)Math.Pow(i + 1, 3);
+// //     }
+// //     return array;
+// // }
+
+//  void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+
