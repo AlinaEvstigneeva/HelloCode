749ec615766437f8df0be39d4abe68aaf09d5c69
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2022-12-21 19:48:37 +0300

    added multiplication table

--- Example013_For/Program.cs
+++ Example013_For/Program.cs
@@ -1,12 +1,26 @@
 ﻿string Method4(int count, string text)
 {
-    int i = 0;
     string result = String.Empty;
-    for (i = 0; i < count; i++)
+    for (int i = 0; i < count; i++)
     {
-        rusult = result + text;
+        result = result + text;
     }
     return result;
 }
 string res = Method4(10, "hello");
-Console.WriteLine(res);
+// Console.WriteLine(res);
+
+
+
+
+// Написать таблицу умножения
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");              // $"{} - интерполяция
+    }
+    Console.WriteLine();                                        // добавили пустую строчку между табличками
+}
+
