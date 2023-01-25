5ddc52c18dead4e6770537fc395e8a4993588670
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2023-01-25 22:40:51 +0300

    corrected Example003

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,10 @@
-﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+﻿int numA = 3;               //int - для целых чисел
+int numB = 5;
+int res = numA + numB;     // res - промежуточная переменная, выступающая местом для сохранения результата
+Console.WriteLine(res);
+
+
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9    
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
 Console.WriteLine(numberB);
