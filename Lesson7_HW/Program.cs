775b5696ce6b6f5ed8f77285a9e0e9abcca21890
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2023-03-04 20:42:10 +0300

    added

--- Lesson7_HW/Program.cs
+++ Lesson7_HW/Program.cs
@@ -142,14 +142,14 @@ FindValue(matrix, firstPosition, secondPosition ); */
 
 int[,] GetMatrix()
 {
-    int[,] matrix = new int[3,4];
+    int[,] matrix = new int[3, 4];
     Random rnd = new Random();
 
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i,j] = rnd.Next(1,10);
+            matrix[i, j] = rnd.Next(1, 10);
         }
     }
 
@@ -162,14 +162,31 @@ void PrintMatrix(int[,] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matrix[i,j]} ");
+            Console.Write($"{matrix[i, j]} ");
         }
         Console.WriteLine();
     }
 }
 
+/*void FindAverage(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        Console.WriteLine($"{sum}");
+    }
+}*/
+
+
+
 
 int[,] matrix = GetMatrix();
 PrintMatrix(matrix);
+/*FindAverage(matrix);*/
+
 
 
